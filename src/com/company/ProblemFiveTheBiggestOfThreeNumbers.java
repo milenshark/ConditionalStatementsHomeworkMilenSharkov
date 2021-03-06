cc58c495a2d94d13a9a192c71d46a0f3e384cbac
Milen Shark <milen.shark@gmail.com>
2021-03-06 19:14:50 +0200

    Create ProblemFiveTheBiggestOfThreeNumbers.java

---
+++ src/com/company/ProblemFiveTheBiggestOfThreeNumbers.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ProblemFiveTheBiggestOfThreeNumbers {
+    public static void main(String[] args) {
+
+//        Problem 5. The Biggest of 3 Numbers
+//        Write a program that finds the biggest of three numbers.
+
+        Scanner scanner = new Scanner(System.in);
+        double a = Double.parseDouble(scanner.nextLine());
+        double b = Double.parseDouble(scanner.nextLine());
+        double c = Double.parseDouble(scanner.nextLine());
+
+        if (a > b )
+
+    }
+}
