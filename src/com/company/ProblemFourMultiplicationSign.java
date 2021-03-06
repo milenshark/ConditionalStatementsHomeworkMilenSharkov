f74eac953cecd78f08e0ac9755097625fd767553
Milen Shark <milen.shark@gmail.com>
2021-03-06 17:41:27 +0200

    Resolving Problem four. Used sequence of if operators to handle all the cases.

--- src/com/company/ProblemFourMultiplicationSign.java
+++ src/com/company/ProblemFourMultiplicationSign.java
@@ -12,12 +12,27 @@ public class ProblemFourMultiplicationSign {
 
         Scanner scanner = new Scanner(System.in);
         double a = Double.parseDouble(scanner.nextLine());
-        double b = scanner.nextDouble();
-        double c = scanner.nextDouble();
+        double b = Double.parseDouble(scanner.nextLine());
+        double c = Double.parseDouble(scanner.nextLine());
 
-        if(a == 0 || b == 0 || c == 0) {
+        if (a == 0 || b == 0 || c == 0) {
             System.out.println("0");
+        } else if (a > 0 && b > 0 && c > 0) {
+            System.out.println("+");
+        } else if (a < 0 && b > 0 && c > 0 ) {
+            System.out.println("-");
+        } else if (a > 0 && b < 0 && c > 0 ) {
+            System.out.println("-");
+        } else if (a > 0 && b > 0 && c < 0 ) {
+            System.out.println("-");
+        } else if (a < 0 && b > 0 && c < 0 ) {
+            System.out.println("+");
+        } else if (a < 0 && b < 0 && c > 0 ) {
+            System.out.println("+");
+        } else if (a > 0 && b < 0 && c < 0 ) {
+            System.out.println("+");
+        } else if (a < 0 && b < 0 && c < 0 ) {
+            System.out.println("-");
         }
-
     }
 }
